3d4d323c16d38753a2bcf3250bc1385fc6565148
Stewart <jmstewart@paypal.com>
2020-01-10 12:52:33 -0800

    Some git cleanup

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const { PORT = 8080 } = process.env;
 import express from 'express';
+import helmet from 'helmet';
 import compression from 'compression';
 import morgan from 'morgan';
 import cookies from 'cookie-parser';
 import router from './server/router';
 const app = express();
+process.env.PRODUCTION && app.use(helmet());
 app.use(compression());
 app.use(morgan('dev'));
 app.use(cookies());
