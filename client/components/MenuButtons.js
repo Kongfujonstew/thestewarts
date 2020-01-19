6681b6a16457733ab20b16d1bccac839a4cfcfa7
Jonathan Stewart <jonstew@gmail.com>
2020-01-19 11:18:28 -0800

    Finish MenuButtons

--- client/components/MenuButtons.js
+++ client/components/MenuButtons.js
@@ -2,20 +2,23 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 
 class MenuButtons extends React.Component {
-
-  scroll = (nextId) => {
+  scroll = (nextId) => () => {
     const el = document.getElementById(nextId);
     el && el.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  handleClickRegistry = () => {
+    window.open('https://www.zola.com/registry/hermannandjon2020', '_blank');
   }
 
   render() {
     return (
       <div className={this.props.thisClass}>
-        <Button>ABOUT US</Button>
-        <Button>EVENT</Button>
-        <Button>REGISTRY</Button>
+        <Button onClick={this.scroll('about-container')}>OUR STORY</Button>
+        <Button onClick={this.scroll('rsvp-container')}>RSVP</Button>
+        <Button onClick={this.handleClickRegistry}>REGISTRY</Button>
       </div>
-    )
+    );
   }
 }
 
