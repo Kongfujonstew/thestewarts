8cece73531b89fe0c9310ef9bbe4a7c7a38ec5f0
Jonathan Stewart <jonstew@gmail.com>
2020-01-19 15:39:56 -0800

    Location first draft

--- client/components/Location.js
+++ client/components/Location.js
@@ -32,20 +32,18 @@ class Location extends React.Component {
     return container.clientWidth || '300px';
   }
 
-  // Location // Enamel or title
-
-  // The Swedenborgian Church
-  // Click here to learn about the church (https://sfswedenborgian.org/)
-
-  // 2107 Lyon St, San Francisco, CA 94115
-
   render() {
     const { width } = this.state;
 
     return (
       <div id="location">
+        <div className="text">
+          <span>Swedenborgian Church</span>
+          <span>2107 Lyon Street</span>
+          <span>San Francisco, California</span>
+        </div>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3152.987260667965!2d-122.44847528404276!3d37.790338319035705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808580cd2eb55531%3A0xa45d8db1e8a16b7b!2sSWEDENBORGIAN%20CHURCH%20of%20San%20Francisco!5e0!3m2!1sen!2sus!4v1578611280889!5m2!1sen!2sus" frameBorder="0" allowFullScreen={true}
-          style={{ width, height: '60vh', border: 0 }}
+          style={{ width, height: '45vh', border: 0 }}
           className="box-shadow"
         ></iframe>
       </div>
